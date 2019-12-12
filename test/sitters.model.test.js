70541c8182e6ad06ac427948b2aca11d070a8462
Mike M. Fleming <mike.fleming.dev@gmail.com>
2019-12-12 15:55:02 -0600

    Dev upgrades (#1)
    * remove teardown script
    
    * removes knex commands from scripts

--- test/sitters.model.test.js
+++ test/sitters.model.test.js
@@ -1,15 +1,9 @@
-// const { getAllSitters, querySittersByX } = require('../db/sitters.model');
+// const { getAllSitters } = require('../db/models/sitters.model');
 
 describe.skip('Sitters Model', () => {
   it('#getAllSitters', async () => {
     const allSitters = await getAllSitters();
 
     expect(allSitters.length).toBe(3);
-  });
-
-  it('#querySittersByX', async () => {
-    const cats = await querySittersByX({ name: 'lorenzo' });
-
-    expect(cats.length).toBe(4);
   });
 });
--- test/teardown.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const db = require('../db');
-
-const dbPath = path.join(__dirname, '../db');
-
-module.exports = async () => {
-  await db.migrate.rollback({ directory: `${dbPath}/migrations` }, true);
-};
