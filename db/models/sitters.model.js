d45094d12a68400bd38c85ce5d09ddedbf34a8a0
Kimberly Bui <kimberlybui@AU-KBUI-MBP15.local>
2019-12-13 10:56:28 -0600

    add query sitter and add sitter

--- db/models/sitters.model.js
+++ db/models/sitters.model.js
@@ -3,3 +3,10 @@ const db = require('..');
 const tableName = 'sitters';
 
 exports.getAllSitters = () => db.select().table(tableName);
+
+exports.querySitters = (query) => db(tableName).where(query);
+
+exports.addSitter = (sitter) => db(tableName)
+  .insert(sitter)
+  .returning('id')
+  .then((data) => data[0]);
--- test/sitters.model.test.js
+++ test/sitters.model.test.js
@@ -1,4 +1,8 @@
-const { getAllSitters } = require('../db/models/sitters.model');
+const {
+  getAllSitters,
+  querySitters,
+  addSitter,
+} = require('../db/models/sitters.model');
 
 describe('Sitters Model', () => {
   it('#getAllSitters', async () => {
@@ -6,4 +10,24 @@ describe('Sitters Model', () => {
 
     expect(allSitters.length).toBe(3);
   });
+
+  it('#querySitters', async () => {
+    const jls = await querySitters({
+      preferred_pet: 'rufus',
+    });
+    expect(jls[0].name).toBe('jls');
+  });
+
+  it('#addSitter', async () => {
+    const id = await addSitter({
+      name: 'katy',
+      preferred_pet: 'Obie',
+    });
+    const katy = await querySitters({ name: 'katy' });
+    expect(typeof id).toBe('number');
+    expect(Array.isArray(katy)).toBe(true);
+    expect(katy[0].preferred_pet).toBe('Obie');
+  });
+
+  it.skip('#updateSitter', async () => {});
 });
